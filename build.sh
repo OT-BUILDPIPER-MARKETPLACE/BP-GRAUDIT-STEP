03ca57044be2f189c7e4e49ecfd291d4ae7fa139
Priyanshi Chauhan <107524071+Cpriyanshi77@users.noreply.github.com>
2023-03-13 00:21:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ source aws-functions.sh
 
 CODE="$WORKSPACE/$CODEBASE_DIR"
 
-logInfoMessage "I'll run pytest on the test suite for $CODE to ensure that all functionality is working as expected.."
+logInfoMessage "I'll scan the CODEbase $CODE for vulnerabilities using Graudit to ensure the application's security."
 sleep $SLEEP_DURATION
 
 if [ -d "reports" ]; then
@@ -19,35 +19,32 @@ fi
 
 if [ -d $CODE ]; then
     logInfoMessage "Executing command"
-    logInfoMessage "pytest --color=yes --code-highlight=yes --show-capture=all --junit-xml=reports/$OUTPUT_ARG"
-    cd $CODE
-    pytest --color=yes --code-highlight=yes --show-capture=all  --junit-xml=reports/$OUTPUT_ARG
+    logInfoMessage "graudit -A -d /usr/share/graudit/$LANGUAGE.db $CODE"
+    graudit -A -d /usr/share/graudit/$LANGUAGE.db $CODE  > reports/$OUTPUT_ARG
 
-exit_code=$?
-
-    if [ $exit_code -ne 0 ]; then
+    if [ $(wc -l < reports/$OUTPUT_ARG) -gt 10 ]; then
         if [ "$VALIDATION_FAILURE_ACTION" == "FAILURE" ]
         then
-            logErrorMessage "Pytest unit test failed! Please review the error messages and update the code as necessary."
-            generateOutput $ACTIVITY_SUB_TASK_CODE false "pytest tests failed! Please review the error messages and update the code as necessary."
-            logErrorMessage "Please review the Pytest report for more information."
+            logErrorMessage "Graudit scan found potential security vulnerabilities."
+            generateOutput $ACTIVITY_SUB_TASK_CODE false "Graudit scan found potential security vulnerabilities."
+            logErrorMessage "Please review the Graudit scan report for more information."
+            logErrorMessage "Please check the Git repository Graudit vulnerabilities scan failed.!!!"
+            cat "reports/$OUTPUT_ARG"
             exit 1
         else
-            logErrorMessage "Pytest unit test failed! Please review the error messages and update the code as necessary."
-            generateOutput $ACTIVITY_SUB_TASK_CODE false "Pytest unit test failed! Please review the error messages and update the code as necessary."
-            logWarningMessage "Please review the Pytest report for more information."
+            logErrorMessage "Graudit scan found potential security vulnerabilities."
+            generateOutput $ACTIVITY_SUB_TASK_CODE false "Graudit scan found potential security vulnerabilities."
+            logWarningMessage "Please review the Graudit scan report for more information."
+            logWarningMessage "Please check the Git repository Graudit vulnerabilities scan failed.!!!"
+            cat "reports/$OUTPUT_ARG"
         fi
     else
-        logInfoMessage "Congratulations Pytest unit test succeeded."
-        generateOutput $ACTIVITY_SUB_TASK_CODE true "Congratulations Pytest unit test succeeded."
+        logInfoMessage "Congratulations Graudit scan succeeded with no vulnerabilities found."
+        generateOutput $ACTIVITY_SUB_TASK_CODE true "Congratulations Graudit scan succeeded with no vulnerabilities found."
     fi
 else
-    logErrorMessage "$CODE: Codebase directory is not found"
-    generateOutput $ACTIVITY_SUB_TASK_CODE false "$CODE Codebase directory is not found."
-    logErrorMessage "Please check the Git repository Pytest unit test failed.!!!"
+    logErrorMessage "$CODE: CODEbase directory is not found"
+    generateOutput $ACTIVITY_SUB_TASK_CODE false "$CODE CODEbase directory is not found."
+    logErrorMessage "Please check the Git repository Graudit vulnerabilities scan failed.!!!"
 fi
-
-
-
-
 
